549f1274d925a75e290c24af4aad16fec978e86d
Pavanmuthyam123 <124612204+Pavanmuthyam123@users.noreply.github.com>
2024-07-22 21:19:40 +0530

    Add files via upload

---
+++ Spring_Boot-CRUD/src/main/java/com/operation/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.operation.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.operation.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Product findByName(String name);
+}
---
+++ Spring_Boot-CRUD/src/main/java/com/product/CrudOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudOperationsApplication.class, args);
+	}
+
+}
